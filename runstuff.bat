0b4438fb4acc5ec16ed6dcd4a3a526972e7ffc38
ArmenZz <45766423+ArmenZz@users.noreply.github.com>
2020-07-09 19:07:16 +0300

    Included batch file for multiple instances

---
+++ runstuff.bat
@@ -0,0 +1,10 @@
+start python ibot1.py 
+start python ibot2.py 
+start python ibot3.py 
+start python ibot4.py 
+start python ibot5.py
+start python ibot9.py
+start python ibot10.py
+start python ibot11.py
+start python ibot12.py 
+
